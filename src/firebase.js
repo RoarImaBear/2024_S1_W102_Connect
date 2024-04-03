6c4e6cb227447b9a40eb55c8c43ccbea7a9f8fb8
Seb <143362916+RoarImaBear@users.noreply.github.com>
2024-04-04 10:53:07 +1300

    Creating Base
    Firebase Database Connected

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyChY0_7dhC0cnhO8RyrAOaywvkBKB3zO0U",
+    authDomain: "aut-connect-8d15c.firebaseapp.com",
+    projectId: "aut-connect-8d15c",
+    storageBucket: "aut-connect-8d15c.appspot.com",
+    messagingSenderId: "109209299656",
+    appId: "1:109209299656:web:9c9216df13e106f838d3fd",
+    measurementId: "G-49BXD92D8C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
