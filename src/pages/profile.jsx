f90f972d4feb005c1f2c660f6f78900ba77cc930
Seb <143362916+RoarImaBear@users.noreply.github.com>
2024-04-05 10:58:35 +1300

    Added Profile Page

---
+++ src/pages/profile.jsx
@@ -0,0 +1,16 @@
+import React, {useRef} from 'react'
+import { firestore } from "../firebase"
+import { addDoc, collection} from "@firebase/firestore"
+
+export default function Home(){
+
+
+    return(
+
+        <body>
+            <section className="main-container">
+                
+            </section>
+        </body>
+    )
+}
