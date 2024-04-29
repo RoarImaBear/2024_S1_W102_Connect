2012ff22297de264d7a1ba451175e7b3fbf65f1e
phoebemichelle <phoebemichelle012@gmail.com>
2024-04-29 15:23:25 +1200

    Merging from main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
 import Home from "./pages/home";
-import SignUpForm from './pages/SignUpForm';
-import LoginForm from './pages/LoginForm';
+import SignUpForm from "./pages/SignUpForm";
+import LoginForm from "./pages/LoginForm";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Profile from "./pages/profile";
 import Feed from "./pages/feed";
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-
 
 function App() {
   return (
@@ -16,6 +15,7 @@ function App() {
         <Route path="/login" element={<LoginForm />} />
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/feed" element={<Feed />} />
       </Routes>
     </BrowserRouter>
   );
